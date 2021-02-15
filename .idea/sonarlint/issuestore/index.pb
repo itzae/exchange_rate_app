0f483b85c21fc67b0f7f2993f433270ce7c63fb7
Itzae <itzaeg@gmail.com>
2021-02-15 03:55:32 -0600

    Task: #4 It was added splash screen

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,8 +1,6 @@
 
 R
 "app/src/main/res/values/colors.xml,9\6\969faa2a5d9f5e8390e958d2c07f2cfd54676e6b
-R
-"app/src/main/res/values/themes.xml,e\b\eb7f0f821bde169dd21862705b2b042d2cea69a3
 X
 (app/src/main/res/values-night/themes.xml,8\c\8c3a6b96d9ffba496992e612d58227c19e6b9e65
 m
---
+++ app/src/main/java/com/itgonca/exchangerateapp/features/splash/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.itgonca.exchangerateapp.features.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.itgonca.exchangerateapp.MainActivity
+import com.itgonca.exchangerateapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+        }, 3000)
+    }
+}
